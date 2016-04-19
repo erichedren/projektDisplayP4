c73bd7d4324aa24ba97974aaa87a9198d008e351
erichedren <eric.hedren@gmail.com>
2016-04-19 12:28:20 +0200

    Added files via upload

--- button.php
+++ button.php
@@ -4,58 +4,32 @@
 	</head>
 	<body>
 		<div class="col-3">
-			<a href= '?test()' class="button_on">Test</a>
+			<a href= "?button_on" class="button_on">On</a>
 		</div>
 		<div class="col-3">
-			<a href= "button_off();" class="button_off">Off</a>
+			<a href= "?button_off" class="button_off">Off</a>
 		</div>
 	</body>
 </html>
 <?php 
-
+	if (isset($_GET['button_on'])){
+		button_on();
+	}
+	if (isset($_GET['button_off'])){
+		button_off();
+	}
 	function button_on() {
-		$divices = tdGetNumberOfDevices();
-		printf("Devices: %d\n" + $devices);
-		
-		$allMethods = TELLDUS_TURNON;
-		
-		for ($i = 0; $i < $devices; ++$i) {
-				
-				$allMethods = TELLDUS_TURNON;
-				$id = tdGetDeviceId($i);
-				$name = utf8_encode(tdGetName($id));
-				printf("%s - %s\n", $id, $name);
-				
-				$methods = tdMethods($id, $allMethods);
-				if($methods & TELLDUS_TURNON) {
-					echo " * TurnOn\n";
-					tdTurnOn( $id );
-						sleep(1);
-				}		
-		}
+			echo " * TurnedOn\n";
+			$resOn = shell_exec("tdtool --on 2");
+				sleep(1);
+			$resOn = shell_exec("tdtool --on 2");
+				sleep(1);
 	}
 	function button_off() {
-		$divices = tdGetNumberOfDevices();
-		printf("Devices: %d\n" + $devices);
-		
-		$allMethods = TELLDUS_TURNOFF;
-		
-		for ($i = 0; $i < $devices; ++$i) {
-			
-				$id = tdGetDeviceId($i);
-				$name = utf8_encode(tdGetName($id));
-				printf("%s - %s\n", $id, $name);
-				
-				$methods = tdMethods($id, $allMethods);
-				if($methods & TELLDUS_TURNOFF) {
-					echo " * TurnOf\n";
-					tdTurnOn( $id );
-						sleep(1);
-				}		
-		}
+			echo " * TurnedOf\n";
+			$resOf = shell_exec("tdtool --off 2");
+				sleep(1);
+			$resOf = shell_exec("tdtool --off 2");
+				sleep(1);		
 	}
-	function test() {
-		echo "Din mamma";
-	}
-
 ?>
