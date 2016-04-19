7b43d083584f64a8d4f7cb5989a9f1e02d36330a
erichedren <eric.hedren@gmail.com>
2016-04-19 08:30:54 +0200

    Added files via upload

--- button.php
+++ button.php
@@ -5,15 +5,31 @@
 	<body>
 		<div class="col-12">
 			<div class = "button" id="on_button">
-				<p class ="button"> On </p>
+				<input type="submit" name="On" value="On" onclick="button_on()" />
 			</div>
 		</div>
 	</body>
 </html>
 <?php 
-	if(!extension_loaded('telldus') {
-		dl('telldus' . PHP_SHL_SUFFIX);
+	function button_on() {
+		if(!extension_loaded('telldus')
+			dl('telldus' . PHP_SHL_SUFFIX);
+		
+		$divices = tdGetNumberOfDevices();
+		printf("Devices: %d\n" + $devices);
+		
+		for ($i = 0; $i < $devices; ++$i) {
+			
+				$id = tdGetDeviceId($i);
+				$name = utf8_encode(tdGetName($id));
+				printf("%s - %s\n", $id, $name);
+				
+				$methods = tdMethods($id, $allMethods);
+				if($methods & TELLDUS_TURN_ON) {
+					echo " * TurnOn\n";
+					tdTurnOn( $id );
+						sleep(1);
+				}		
+		}
 	}
-	
-	$divices = tdGetNumberOfDevices();
-	printf("Devices")
+?>
