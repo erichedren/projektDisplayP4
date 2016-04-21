9a56f65f50f90cc0228be8cd97f58a962dafd197
Henrik Strand <henkestr@gmail.com>
2016-04-21 16:55:04 +0200

    Added files via upload

--- projektDisplayP4/resources/fragments/header.php
+++ projektDisplayP4/resources/fragments/header.php
@@ -1,12 +1,12 @@
 <html>
 	<head>
-		<div class="col-12">
-			<div class="col-3">
-			</div>
-			<div class="col-9">
-				<p> Grupp 8 - Smart Display </p>
-			</div>
-		</div>
+		<div class="col-12" id="head">
+                <div class="col-3">
+                </div>
+                <div class="col-9">
+                    <H1> Grupp 8 - Smart Display </H1>
+                </div>
+            </div>    
 	</head>
 	<body>
 	</body>
