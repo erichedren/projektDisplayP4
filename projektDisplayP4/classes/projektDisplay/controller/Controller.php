8584a2240b992a83a09a914eae1f7c61f527635c
erichedren <eric.hedren@gmail.com>
2016-04-26 11:07:08 +0200

    Added files via upload

--- projektDisplayP4/classes/projektDisplay/controller/Controller.php
+++ projektDisplayP4/classes/projektDisplay/controller/Controller.php
@@ -2,10 +2,12 @@
 	namespace projektDisplay\controller;
 	use projektDisplay\model\Login;
     use projektDisplay\integration\LoginHandler;
+	use projektDisplay\integration\DateMessageHandler;
     class Controller {
 		private $loginHand;
 		public function __construct() {
 			 $this->loginHand = new LoginHandler();
+			 $this->dateMessageHand = new DateMessageHandler();
         }
 		
         public function logIn(Login $login){
@@ -14,4 +16,7 @@
 		public function logOut() {
             $this->loginHand->logOut();
         }
+		public function addMessage($message,$dateStart,$dateStop){
+			$this->dateMessageHand->addMessage($message,$dateStart,$dateStop);
+		}
     }
