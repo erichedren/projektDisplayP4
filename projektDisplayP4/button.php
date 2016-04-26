e01cc8a3803b88564221bad8064ac559a5df136b
erichedren <eric.hedren@gmail.com>
2016-04-26 11:06:38 +0200

    Added files via upload

--- projektDisplayP4/button.php
+++ projektDisplayP4/button.php
@@ -1,6 +1,7 @@
 
 <?php 
-    
+    $controller = SessionManager::getController();
+    sessionManager::storeController($controller);
 	if (isset($_GET['button_on'])){
 		button_on();
 	}
@@ -8,7 +9,7 @@
 		button_off();
 	}
 	if (isset($_GET['formSubmit'])){
-		dim($Value = $_GET['dim']);
+		dim($Value = $_GET['dim'],$start = $_GET['start'], $stop = $_GET['stop']);
 	}
 	function button_on() {
 			echo " * TurnedOn\n";
@@ -24,11 +25,13 @@
 			$resOf = shell_exec("tdtool --off 2");
 				sleep(1);		
 	}
-    function dim($V){
-              $Value = $V * 15;
+    function dim($V,$start,$stop){
+            $Value = $V * 15;
             
-              echo "$Value";
-             $resOn = shell_exec("tdtool --dimlevel $Value  --dim 2");
-                sleep(1);
+            echo "$Value";
+            $resOn = shell_exec("tdtool --dimlevel $Value  --dim 2");
+            sleep(1);
+			$controller->addMessage($V,$start,$stop);
+			
   }
 ?>
--- projektDisplayP4/start.php
+++ projektDisplayP4/start.php
@@ -2,33 +2,36 @@
 	<body>
 		<p> Turn the telldus device on or off </p>
 		
-		<div class="col-5">
+		<div class="col-12">
 			<a href= "?button_on" class="button_on"><i class="fa fa-play fa-fw"></i> On</a>
 			
 			<a href= "?button_off" class="button_off"><i class="fa fa-ban fa-fw"></i> Off</a>
-		</div>
-		<div class="col-7">
+			<br><br>
 		
-			<form action="">
-				<input type="checkbox" name="dim" value="1">DimLevel 1<br>
-				<input type="checkbox" name="dim" value="2">DimLevel 2 <br>
-                <input type="checkbox" name="dim" value="3">DimLevel 3<br>
-				<input type="checkbox" name="dim" value="4">DimLevel 4 <br>
-                <input type="checkbox" name="dim" value="5">DimLevel 5 <br>
-                <input type="checkbox" name="dim" value="6">DimLevel 6 <br>
-                <input type="checkbox" name="dim" value="7">DimLevel 7 <br>
-                <input type="checkbox" name="dim" value="8">DimLevel 8 <br>
-                <input type="checkbox" name="dim" value="9">DimLevel 9 <br>
-                <input type="checkbox" name="dim" value="10">DimLevel 10 <br>
-                <input type="checkbox" name="dim" value="11">DimLevel 11 <br>
-                <input type="checkbox" name="dim" value="12">DimLevel 12 <br>
-                <input type="checkbox" name="dim" value="13">DimLevel 13 <br>
-                <input type="checkbox" name="dim" value="14">DimLevel 14 <br>
-                <input type="checkbox" name="dim" value="15">DimLevel 15 <br>
-                <input type="checkbox" name="dim" value="16">DimLevel 16 <br>
-                <input type="submit" name="formSubmit" value="Submit" />
-			</form>
-			
+			<fieldset>
+			<legend>Messages:</legend>
+				<form action="">
+					<input type="checkbox" name="dim" value="1">DimLevel 1<br>
+					<input type="checkbox" name="dim" value="2">DimLevel 2 <br>
+					<input type="checkbox" name="dim" value="3">DimLevel 3<br>
+					<input type="checkbox" name="dim" value="4">DimLevel 4 <br>
+					<input type="checkbox" name="dim" value="5">DimLevel 5 <br>
+					<input type="checkbox" name="dim" value="6">DimLevel 6 <br>
+					<input type="checkbox" name="dim" value="7">DimLevel 7 <br>
+					<input type="checkbox" name="dim" value="8">DimLevel 8 <br>
+					<input type="checkbox" name="dim" value="9">DimLevel 9 <br>
+					<input type="checkbox" name="dim" value="10">DimLevel 10 <br>
+					<input type="checkbox" name="dim" value="11">DimLevel 11 <br>
+					<input type="checkbox" name="dim" value="12">DimLevel 12 <br>
+					<input type="checkbox" name="dim" value="13">DimLevel 13 <br>
+					<input type="checkbox" name="dim" value="14">DimLevel 14 <br>
+					<input type="checkbox" name="dim" value="15">DimLevel 15 <br>
+					<input type="checkbox" name="dim" value="16">DimLevel 16 <br>
+					<input type="date" name="start" required>Start date <br>
+					<input type="date" name="stop" required>Stop date <br>
+					<input type="submit" name="formSubmit" value="Submit" />
+				</form>
+			</fieldset>			
 		</div>
 		<?php 
 		//Functions for the buttons functionallity
