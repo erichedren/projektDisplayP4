fb2c482c18e9d7e36373372d334b8d6cf51a418e
riille <rickard.e94@gmail.com>
2016-04-27 16:32:59 +0200

    Added files via upload

--- projektDisplayP4/classes/projektDisplay/controller/Controller.php
+++ projektDisplayP4/classes/projektDisplay/controller/Controller.php
@@ -5,9 +5,15 @@
 	use projektDisplay\integration\DateMessageHandler;
     class Controller {
 		private $loginHand;
+		
+		private $IP = "127.0.0.1";
+		private $db_user = "root";
+		private $db_password = "pass";
+		private $db = "grupp8";
+		
 		public function __construct() {
-			 $this->loginHand = new LoginHandler();
-			 $this->dateMessageHand = new DateMessageHandler();
+			 $this->loginHand = new LoginHandler($this->IP, $this->db_user, $this->db_password, $this->db);
+			 $this->dateMessageHand = new DateMessageHandler($this->IP, $this->db_user, $this->db_password, $this->db);
         }
 		
         public function logIn(Login $login){
@@ -16,7 +22,15 @@
 		public function logOut() {
             $this->loginHand->logOut();
         }
-		public function addMessage($message,$dateStart,$dateStop){
-			$this->dateMessageHand->addMessage($message,$dateStart,$dateStop);
+		public function addMessage($message){
+			$this->dateMessageHand->addMessage($message);
+		}
+		
+		public function addMessageWithDate($message,$dateStart,$dateStop){
+			$this->dateMessageHand->addMessageWithDate($message,$dateStart,$dateStop);
+		}
+		
+		public function getMessage($V){
+			return $this->dateMessageHand->getMessage($V);
 		}
     }
