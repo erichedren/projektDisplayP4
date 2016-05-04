bef52032f8cd04d301793f9ec431c7af1aa829c7
Henrik Strand <henkestr@gmail.com>
2016-05-04 10:10:26 +0200

    Update menu.php

--- projektDisplayP4/resources/fragments/menu.php
+++ projektDisplayP4/resources/fragments/menu.php
@@ -3,7 +3,7 @@
 <div class="menu">
 	<ul id="menu">
 		<li><a href="?page=start"><i class="fa fa-home fa-fw"></i> Home</a></li>
-        <li><a href="?page=calendar"><i class="fa fa-calendar fa-fw"></i> Calender</a></li>
+        <li><a href="?page=calendar"><i class="fa fa-calendar fa-fw"></i> Calendar</a></li>
 		<li><a href="?page=settings"><i class="fa fa-cogs fa-fw"></i> Settings</a></li>
 		<li><?php include ('logged_in.php'); ?></li>
 	</ul>
